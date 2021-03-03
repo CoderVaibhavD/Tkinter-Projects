8629d8a0845b164842a50f2ba080c48425cf624d
CoderVaibhavD <79995781+CoderVaibhavD@users.noreply.github.com>
2021-03-03 17:23:49 +0000

    Add files via upload

---
+++ Jumble.py
@@ -0,0 +1,35 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Vaibhav
+#
+# Created:     03/03/2021
+# Copyright:   (c) Vaibhav 2021
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+from tkinter import *
+from random import choice, shuffle
+
+root = Tk()
+root.title('Game1')
+root.geometry('800x600')
+
+def shuffle():
+
+#End function
+
+def theanswer():
+
+#End function
+
+def hint():
+
+#End function
+
+
+def initialize():
+    shuffle()
+#End function
+
+root.mainloop()
